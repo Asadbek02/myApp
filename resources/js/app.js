e1ff926cf8ad28a0fee4c59b78607f6d07b443fc
asadbek02 <asadbekabdusalomov2076@gmail.com>
2024-02-15 14:32:56 +0500

    project created

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import '@/index.css';
+import {createApp} from "vue";
+import App from "@/App.vue";
+import Router from "@/router/router.js";
+
+createApp(App).use(Router).mount('#app');
---
+++ resources/js/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <ul>
+        <li><router-link :to="{ name: 'home' }">Home</router-link></li>
+        <li><router-link :to="{ name: 'about' }">About</router-link></li>
+    </ul>
+        <router-view />
+</template>
+
+<style scoped>
+
+</style>
